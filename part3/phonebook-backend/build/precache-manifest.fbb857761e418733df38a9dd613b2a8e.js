b3f8743c7eb11023ec80ad24e3fad39c10ece88a
Jacob Hughey <jcbhghy@gmail.com>
2020-04-12 13:51:11 -0400

    Added setup for heroku

---
+++ part3/phonebook-backend/build/precache-manifest.fbb857761e418733df38a9dd613b2a8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d704e7ae34f5f44a73f2ec7b26ff94e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59fff4fe03c728736628",
+    "url": "/static/css/main.4c7a5f18.chunk.css"
+  },
+  {
+    "revision": "f5ffb0dc48ad9952bcfd",
+    "url": "/static/js/2.5c68fe71.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5c68fe71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59fff4fe03c728736628",
+    "url": "/static/js/main.ab2662c6.chunk.js"
+  },
+  {
+    "revision": "cbe135e57de162cd240f",
+    "url": "/static/js/runtime-main.d0b0a789.js"
+  }
+]);
--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+const cors = require('cors')
 
 app.use(express.json())
 app.use(morgan('tiny'))
+app.use(cors())
+app.use(express.static('build'))
 
 let persons = [
     {
@@ -98,7 +101,7 @@ app.post('/api/persons', (req, res) => {
     res.json(person)
 })
 
-
-const port = 3001
-app.listen(port)
-console.log(`Server running on port ${port}`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
